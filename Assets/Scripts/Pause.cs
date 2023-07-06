8bbf108c38fb45d84e77393b413057a04a3e7add
jovan <jovan.stojanovic36@gmail.com>
2023-07-06 20:18:13 +0200

    Adding the whole game

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+
+    public static bool isGamePaused = false;
+    public SupermanScript logic;
+
+    // Update is called once per frame
+
+    [SerializeField]
+    GameObject pauseMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && logic.supermanIsAlive)
+        {
+            if(isGamePaused)
+            {
+                ResumeGame();
+            } else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void OpenMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame() { 
+        Application.Quit();
+
+        Debug.Log("Quit");
+    }
+}
